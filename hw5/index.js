b4e37462c3024831639acbe02a90d576259c18a4
Iryna Burlaka <i.am.blinova@gmail.com>
2025-07-02 12:52:44 +0200

    hw5

--- hw5/index.js
+++ hw5/index.js
@@ -1,21 +1,74 @@
 // ====== Advanced Array Filtering ======
 
 function customFilterUnique(value, fn) {
-  const arr = new Set();
+  const arr = [];
   console.log(arr);
   //   const result = [];
 
   for (const el of value) {
-    const element = fn(el);
+    // const element = fn(el);
 
     //check el on unique
-    if (!arr.has(element)) {
-      arr.add(element);
-    }
-    console.log(arr);
+    // if (!arr.has(el)) {
+    //   arr.add(el);
+    // }
+    if (fn(el)) arr.push(el);
   }
   return arr;
 }
-const numbers = [1, 2, 3, 4, 4];
-const arrayFiltered = (numbers, (item) => item);
-customFilterUnique(numbers);
+const numbers = [1, 2, 3, 4, 4, 1, 3, 5];
+const people = [
+  {
+    name: "igor",
+    age: 37,
+  },
+  {
+    name: "Iryna",
+    age: 42,
+  },
+];
+// const arrayFiltered = (el) => el >= 3;
+const arrayFiltered = (el) => el.age >= 38;
+
+const a = customFilterUnique(people, arrayFiltered);
+// console.log(a);
+// console.log(numbers.filter((el) => el >= 3));
+
+// ======= Array Chunking ========
+
+/*
+Create a function called chunkArray that takes an array and a chunk size as arguments. 
+The chunkArray function should divide the array into smaller arrays, 
+each containing elements of the specified chunk size. The function should return an array of arrays.
+*/
+console.log("============================");
+function chunkArray(arr, x) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i += x) {
+    result.push(arr.slice(i, i + x));
+  }
+
+  return result;
+}
+const x = 2;
+let aa = chunkArray(numbers, x);
+console.log(JSON.stringify(aa));
+
+//Optimize the chunkArray function to minimize memory usage while chunking the array.
+
+// function chunkArrayOptimize(arr, x) {
+//   let result = [];
+
+//   for (let i = 0; i < arr.length; i += x) {
+//     const chunk = arr.slice(i, i + x);
+//     result.push(chunk);
+//   }
+
+//   return result;
+// }
+
+// let ff = chunkArrayOptimize(numbers, x);
+// console.log(JSON.stringify(ff));
+
+//============ Array Shuffling ============
